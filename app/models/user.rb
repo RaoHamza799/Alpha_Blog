454e1d4b8198952942eab2ca8130da69a118f184
raohamza <rao.hamza@devbox.co>
2021-12-09 10:31:24 +0000

    added edit colum and also added gender and age

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,31 @@
 class User< ApplicationRecord
     before_save { self.email = email.downcase }
-
     has_many :articles
+    
+    validates :age, numericality: { less_than_or_equal_to: 100,  only_integer: true }
+
+   
+    validates :gender, presence: true, 
+    length: { minimum: 1 , maximum: 1}
+
     validates :username, presence: true, 
     uniqueness: { case_sensitive: false }, 
     length: { minimum: 3, maximum: 25 }
+    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
+    
     validates :email, presence: true, 
     uniqueness: { case_sensitive: false }, 
     length: { maximum: 105 },
     format: { with: VALID_EMAIL_REGEX }
+  
 
       has_secure_password
-        
+    
+
+
+
+
   end
   
